69580713d0851b1991760b5c61ffb2a5e033eba8
Mufaddal Haidermota <mufaddal16.haidermota@gmail.com>
2020-10-22 23:21:25 +0530

    Update train.py

--- nd00333_AZMLND_Optimizing_a_Pipeline_in_Azure-Starter_Files/train.py
+++ nd00333_AZMLND_Optimizing_a_Pipeline_in_Azure-Starter_Files/train.py
@@ -14,13 +14,16 @@ from azureml.data.dataset_factory import TabularDatasetFactory
 # Data is located at:
 # "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
 
-ds = ### YOUR CODE HERE ###
+data_link = "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
+
+ds = from_delimited_files(data_link)
 
 x, y = clean_data(ds)
 
 # TODO: Split data into train and test sets.
 
-### YOUR CODE HERE ###a
+### YOUR CODE HERE ###
+x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.3)
 
 run = Run.get_context()
 
@@ -50,6 +53,8 @@ def clean_data(data):
 
     y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
     
+    return x_df, y_df
+    
 
 def main():
     # Add arguments to script
@@ -67,6 +72,9 @@ def main():
 
     accuracy = model.score(x_test, y_test)
     run.log("Accuracy", np.float(accuracy))
-
+    
+    os.makedirs('output_model',exist_ok=True)
+    joblib.dump(model,'output_model/model.joblib')
+    
 if __name__ == '__main__':
     main()
