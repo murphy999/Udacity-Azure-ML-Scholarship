a5e0e392aeeac78a628d5a14a2c1c65d7860471e
Mufaddal Haidermota <mufaddal16.haidermota@gmail.com>
2020-12-14 17:18:30 +0530

    Add files via upload

---
+++ nd00333-capstone/starter_file/score.py
@@ -0,0 +1,23 @@
+import os
+import pandas as pd
+import json
+import joblib, pickle
+#from azureml.core import Model
+#from sklearn.externals import joblib
+
+
+def init():
+    global deploy_model
+    model_path = os.path.join(os.getenv('AZUREML_MODEL_DIR'), 'best_fit_automl_model.pkl')
+    deploy_model = joblib.load(model_path)
+
+def run(data):
+    try:
+        temp = json.loads(data)
+        data = pd.DataFrame(temp['data'])
+        result = deploy_model.predict(data)
+        # You can return any data type, as long as it is JSON serializable.
+        return result.tolist()
+    except Exception as e:
+        error = str(e)
+        return error
