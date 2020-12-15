a5acd60d4262db1dd19a3e77ed72715a92739d69
Mufaddal Haidermota <mufaddal16.haidermota@gmail.com>
2020-12-15 13:30:00 +0530

    Update model_training.py

--- nd00333-capstone/starter_file/model_training.py
+++ nd00333-capstone/starter_file/model_training.py
@@ -7,25 +7,16 @@ import pandas as pd
 import argparse
 
 # Machine Learning Libraries
+from sklearn.preprocessing import OneHotEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import roc_auc_score
 import joblib
 
 # Azure Libraries
 from azureml.core.run import Run
 from azureml.data.dataset_factory import TabularDatasetFactory
-#from azureml.core import Workspace, Dataset
 
-#subscription_id = ''
-#resource_group = ''
-#workspace_name = ''
-#workspace = Workspace(subscription_id, resource_group, workspace_name)
-
-#dataset = Dataset.get_by_name(workspace, name='mpg')
-#data = dataset.to_pandas_dataframe()
-
-data_link = ""
+data_link = "https://raw.githubusercontent.com/murphy999/Udacity-Azure-ML-Scholarship/master/nd00333-capstone/starter_file/Telco-Customer-Churn.csv"
 ds = TabularDatasetFactory.from_delimited_files(path=data_link)
 
 def clean_data(data):
@@ -34,17 +25,17 @@ def clean_data(data):
     df = data.to_pandas_dataframe().dropna()
     df['TotalCharges'] = pd.to_numeric(df.TotalCharges, errors='coerce')
 
-    df['Partner'] = pd.Series(np.where(df.Partner.values == 'Yes',1,0))
-    df['Dependents'] = pd.Series(np.where(df.Dependents.values == 'Yes',1,0))
-    df['PaperlessBilling'] = pd.Series(np.where(df.PaperlessBilling.values == 'Yes',1,0))
-    df['PhoneService'] = pd.Series(np.where(df.PhoneService.values == 'Yes',1,0))
-    df['MultipleLines'] = pd.Series(np.where(df.MultipleLines.values == 'Yes',1,0))
-    df['OnlineSecurity'] = pd.Series(np.where(df.OnlineSecurity.values == 'Yes',1,0))
-    df['OnlineBackup'] = pd.Series(np.where(df.OnlineBackup.values == 'Yes',1,0))
-    df['DeviceProtection'] = pd.Series(np.where(df.DeviceProtection.values == 'Yes',1,0))
-    df['TechSupport'] = pd.Series(np.where(df.TechSupport.values == 'Yes',1,0))
-    df['StreamingMovies'] = pd.Series(np.where(df.StreamingMovies.values == 'Yes',1,0))
-    df['StreamingTV'] = pd.Series(np.where(df.StreamingTV.values == 'Yes',1,0))
+    df['Partner'] = df.Partner.apply(lambda s: 1 if s == "Yes" else 0)
+    df['Dependents'] = df.Dependents.apply(lambda s: 1 if s == "Yes" else 0)
+    df['PaperlessBilling'] = df.PaperlessBilling.apply(lambda s: 1 if s == "Yes" else 0)
+    df['PhoneService'] = df.PhoneService.apply(lambda s: 1 if s == "Yes" else 0)
+    df['MultipleLines'] = df.MultipleLines.apply(lambda s: 1 if s == "Yes" else 0)
+    df['OnlineSecurity'] = df.OnlineSecurity.apply(lambda s: 1 if s == "Yes" else 0)
+    df['OnlineBackup'] = df.OnlineBackup.apply(lambda s: 1 if s == "Yes" else 0)
+    df['DeviceProtection'] = df.DeviceProtection.apply(lambda s: 1 if s == "Yes" else 0)
+    df['TechSupport'] = df.TechSupport.apply(lambda s: 1 if s == "Yes" else 0)
+    df['StreamingMovies'] = df.StreamingMovies.apply(lambda s: 1 if s == "Yes" else 0)
+    df['StreamingTV'] = df.StreamingTV.apply(lambda s: 1 if s == "Yes" else 0)
     
     df = pd.get_dummies(df,columns=['gender'],drop_first= True)
     df = pd.get_dummies(df,columns=['InternetService'],drop_first= True)
@@ -53,8 +44,10 @@ def clean_data(data):
 
     y_df = df.pop("Churn").apply(lambda s: 1 if s == "Yes" else 0)
     
-    return x_df,y_df
+    return df,y_df
 
+run = Run.get_context()
+    
 def main():
     # Add arguments to script
     parser = argparse.ArgumentParser()
@@ -71,12 +64,9 @@ def main():
     x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.3,random_state=53)
 
     model = RandomForestClassifier(n_estimators=args.n_estimators, max_depth=args.max_depth, min_samples_split=args.min_samples_split).fit(x_train, y_train)
-    
-    y_prob = model.predict_proba(x_test)
-    y_pred = y_prob[:, 1]
 
-    weighted_auc = roc_auc_score(y_test,y_pred,average='weighted')
-    run.log("AUC_weighted", np.float(weighted_auc))
+    accuracy = model.score(x_test, y_test)
+    run.log("Accuracy", np.float(accuracy))
     
     joblib.dump(model,'./model.joblib')
 
